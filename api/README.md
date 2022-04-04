6e1f6bf9a211c195f9671ecb77c30de3f8c323b3
machi <16322422+MachineShot@users.noreply.github.com>
2022-04-04 20:10:45 +0300

    updated springdoc implementation, added readme

---
+++ api/README.md
@@ -0,0 +1,9 @@
+RESTful API with Spring Boot:
+* PostgreSQL database
+* JPA
+* HATEOAS
+* SpringDoc (OpenAPI 3.0)
+
+Swagger UI:
+
+http://localhost:8080/api/swagger-ui/index.html
--- api/src/main/java/com/gocycle/repository/IndicatorRepository.java
+++ api/src/main/java/com/gocycle/repository/IndicatorRepository.java
@@ -2,9 +2,11 @@ package com.gocycle.repository;
 
 import com.gocycle.entity.Indicator;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(collectionResourceRel = "indicators", path = "indicators")
 public interface IndicatorRepository extends JpaRepository<Indicator, Integer> {
     List<Indicator> findByNameContaining(String name);
 }
